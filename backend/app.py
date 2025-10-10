61a501caf7d90b3e533125dff5a73895c4dd11ce
Sangam Bartaula <114772684+sangambartaula@users.noreply.github.com>
2025-10-10 11:03:51 -0500

    Added base index.html file for home page, added images, updated requirements.txt, and changed app.py to show index.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,27 @@
 # app.py
 from flask import Flask
+from dotenv import load_dotenv
+from flask import send_from_directory
+import os
+import mysql.connector
+
+load_dotenv()
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 
 @app.route('/')
 def home():
-    return 'Hello from Flask!'
+    return send_from_directory('../frontend/public', 'index.html')
+
+@app.route('/assets/<path:filename>')
+def assets(filename):
+    return send_from_directory('../frontend/public/assets', filename)
+
+@app.errorhandler(404)
+def not_found(e):
+    return 'Page not found', 404
 
 if __name__ == "__main__":
     app.run()
